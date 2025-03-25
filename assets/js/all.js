49bde913509c4b41c5cbb187ae34be2853b14825
Lainieeeee <yorin0820@gmail.com>
2025-03-25 11:42:16 +0800

    update:update all.js signUp/loginBtn code

--- assets/js/all.js
+++ assets/js/all.js
@@ -9,46 +9,90 @@ const apiUrl = "https://todoo.5xcamp.us";
 // 「註冊帳號」ボタンを定義
 const signUpBtn = document.getElementById("signUpBtn");
 // 「註冊帳號」をクリックしたときに以下を実行
-signUpBtn.addEventListener("click", async (e) => {
+if (signUpBtn) {
+    signUpBtn.addEventListener("click", async (e) => {
 
-    // フォームのデフォルトの送信を防止
-    e.preventDefault();
+        // フォームのデフォルトの送信を防止
+        e.preventDefault();
 
-    // フォームから入力された情報を取得
-    const email = document.getElementById("email").value;
-    const nickname = document.getElementById("name").value;
-    const password = document.getElementById("password").value;
-    const password02 = document.getElementById("password02").value;
+        // フォームから入力された情報を取得
+        const email = document.getElementById("email").value;
+        const nickname = document.getElementById("name").value;
+        const password = document.getElementById("password").value;
+        const password02 = document.getElementById("password02").value;
 
-    // パスワードが一致しているか確認
-    if (password !== password02) {
-        return alert("密碼不一致");
-    }
-
-    try {
-        // サーバーに対してユーザー情報をPOSTリクエストで送信
-        const response = await fetch(`${apiUrl}/users`, {
-            method: "POST", // POSTメソッドを使う
-            headers: { "Content-Type": "application/json" },  // リクエストボディの形式をJSONに指定
-            body: JSON.stringify({ user: { email, nickname, password } })  // ユーザー情報をJSON形式に変換して送信
-        });
-
-        // サーバーからのレスポンスをJSONとして解析
-        const result = await response.json();
-
-        // レスポンスが正常であればログイン成功メッセージを表示
-        if (response.ok) {
-            alert("登錄成功！");
-            window.location.href = "index.html";  // ログインページにリダイレクト
-        } else {
-            alert(`登錄失敗: ${result.error}`);
+        // パスワードが一致しているか確認
+        if (password !== password02) {
+            return alert("密碼不一致");
         }
-    } catch (error) {
-        console.error(error);
-        alert("登錄時發生錯誤");
-    }
-});
+
+        try {
+            // サーバーに対してユーザー情報をPOSTリクエストで送信
+            const response = await fetch(`${apiUrl}/users`, {
+                method: "POST", // POSTメソッドを使う
+                headers: { "Content-Type": "application/json" },  // リクエストボディの形式をJSONに指定
+                body: JSON.stringify({ user: { email, nickname, password } })  // ユーザー情報をJSON形式に変換して送信
+            });
+
+            // サーバーからのレスポンスをJSONとして解析
+            const result = await response.json();
+
+            // レスポンスが正常であればログイン成功メッセージを表示
+            if (response.ok) {
+                alert("登錄成功！");
+                window.location.href = "index.html";  // ログインページにリダイレクト
+            } else {
+                alert(`登錄失敗: ${result.error}`);
+            }
+        } catch (error) {
+            console.error(error);
+            alert("登錄時發生錯誤");
+        }
+    });
+}
 
 // ============================================
-// 
+// 登入
 // ============================================
+// 「登入」ボタンを定義
+const loginBtn = document.getElementById("loginBtn");
+// 「登入」をクリックしたときに以下を実行
+if (loginBtn) {
+    loginBtn.addEventListener("click", async (e) => {
+
+        // フォームのデフォルトの送信を防止
+        e.preventDefault();
+    
+        // フォームから入力された情報を取得
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+    
+        // 入力内容が空でないかチェック
+        if (!email || !password) {
+            return alert("Email 或者密碼未輸入");
+        }
+    
+        try {
+            // サーバーに対してログイン情報をPOSTリクエストで送信
+            const response = await fetch(`${apiUrl}/users/sign_in`, {
+                method: "POST",  // POSTメソッドを使う
+                headers: { "Content-Type": "application/json" },  // リクエストボディの形式をJSONに指定
+                body: JSON.stringify({ user: { email, password } })  // ログイン情報をJSON形式に変換して送信
+            });
+    
+            // サーバーからのレスポンスをJSONとして解析
+            const result = await response.json();
+    
+            // レスポンスが正常であればログイン成功メッセージを表示
+            if (response.ok) {
+                alert("登錄成功！");
+                window.location.href = "toDoList.html";  // リダイレクト
+            } else {
+                alert(`登錄失敗: ${result.error}`);
+            }
+        } catch (error) {
+            console.error(error);
+            alert("登錄時發生錯誤");
+        }
+    });
+}
