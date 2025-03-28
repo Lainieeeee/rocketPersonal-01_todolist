4ee51f4775d70d516ce824b9cb5c18dda85d17b0
Lainieeeee <yorin0820@gmail.com>
2025-03-28 14:39:01 +0800

    update:update tailwind.config.js update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         black:     "#333333",  // 編輯
         secondary: "#9F9A91",  // 編輯
+        silver:    "#E5E5E5",  // 編輯
         yellow:    "#FFD370",  // 編輯
         pink:      "#D87355",  // 編輯
       },
